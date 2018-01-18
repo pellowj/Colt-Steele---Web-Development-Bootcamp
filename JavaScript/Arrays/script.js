44af22cdd5752d88c53c8c4cc6e35ecbce835ebe
John Pellow <pellowj@aol.com>
2018-01-17 21:52:46 -0600

    Add files via upload

---
+++ JavaScript/Arrays/script.js
@@ -0,0 +1,42 @@
+var todos = ["Buy new Turtle"];
+
+var input = prompt("What would you like to you?");
+
+while (input !== "quit") {
+    // handle input
+    if (input === "list") {
+        listTodos();
+    } else if(input === "new") {
+        addTodo();
+    } else if(input === "delete") {
+        deleteTodo;
+    }
+    // ask again for new input
+    input = prompt("What would you like to you?");
+} 
+console.log("Ok, You quit the app");
+
+function listTodos() {
+    console.log("**********");
+    todos.forEach(function(todo, i) {
+        console.log(i + ": " + todo);
+    });
+    console.log("**********");
+}
+
+function addTodo() {
+    // ask for new todo
+    var newTodo = prompt("Enter new todo");
+    // add to todo array
+    todos.push(newTodo);
+    console.log("Added Todo");
+}
+
+function deleteTodo() {
+    // ask for index of todo to be deleted
+    var index = prompt("Enter index of todo to delete");
+    // delete that todo
+    // splice()
+    todos.splice(index, 1);
+    console.log("Deleted Todo");
+}
